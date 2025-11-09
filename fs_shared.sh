936605347ad684605001713c434214802e541e05
AdvApp <advappsw@gmail.com>
2025-11-09 09:07:42 -0700

    Using show/showx; added check for no backups

--- fs_shared.sh
+++ fs_shared.sh
@@ -26,27 +26,33 @@ select_archive() {
     archives+=("${archive}: $comment")
   done < <( ls -1 "$path/fs" )
 
-  # Get the count of options and increment to include cancel
-  local count="${#archives[@]}"
-  ((count++))
+  if [ ${#archives[@]} -eq 0 ]; then
+    showx "There are no backups on $device"
+  else
+    show "Listing backup files..."
 
-  COLUMNS=1
-  select selection in "${archives[@]}" "Cancel"; do
-    if [[ "$REPLY" =~ ^[0-9]+$ && "$REPLY" -ge 1 && "$REPLY" -le $count ]]; then
-      case ${selection} in
-        "Cancel")
-          # If the user decides to cancel...
-          break
-          ;;
-        *)
-          name="${selection%%:*}"
-          break
-          ;;
-      esac
-    else
-      printx "Invalid selection. Please enter a number between 1 and $count.">&2
-    fi
-  done
+    # Get the count of options and increment to include cancel
+    local count="${#archives[@]}"
+    ((count++))
+
+    COLUMNS=1
+    select selection in "${archives[@]}" "Cancel"; do
+      if [[ "$REPLY" =~ ^[0-9]+$ && "$REPLY" -ge 1 && "$REPLY" -le $count ]]; then
+        case ${selection} in
+          "Cancel")
+            # If the user decides to cancel...
+            break
+            ;;
+          *)
+            name="${selection%%:*}"
+            break
+            ;;
+        esac
+      else
+        showx "Invalid selection. Please enter a number between 1 and $count."
+      fi
+    done
+  fi
 
   echo $name
 }
